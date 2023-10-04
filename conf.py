d00a5265a5a785c1b3e91065db1aacf6fd8f65f2
Martin D. Weinberg <weinberg@astro.umass.edu>
2023-10-04 11:09:46 -0400

    Try to get readthedocs to load the sphinx_rtd_theme

--- conf.py
+++ conf.py
@@ -25,7 +25,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # Turn on figure numering
