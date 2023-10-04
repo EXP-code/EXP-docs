9fecd3c6d428aba9d98864db8994274c996f6e82
Martin Weinberg <martin.weinberg@comcast.net>
2023-10-04 10:35:03 -0400

    Update conf.py
    Trying to fix the apparently deprecated 'sphinx_rtd_theme'.

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@ version = '7.x'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = ['sphinx_design']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst']
