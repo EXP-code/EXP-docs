d508eb862783f38eaaddd02d6aded62c1dae20ad
Martin D. Weinberg <weinberg@astro.umass.edu>
2023-10-04 13:24:31 -0400

    Added logo to sidebar

--- conf.py
+++ conf.py
@@ -26,6 +26,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = 'exp_logo.png'
 html_static_path = ['_static']
 
 # Turn on figure numering
