1fb00ccdfd6cb2676355553ad17a4c205a1a3d3b
Martin D. Weinberg <weinberg@astro.umass.edu>
2024-08-07 14:22:25 -0400

    Try a different path for pyEXP library

--- conf.py
+++ conf.py
@@ -3,9 +3,7 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os, sys
-
-sys.path.append(os.path.abspath('./lib'))
+sys.path.insert(0, os.path.abspath('./lib'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
