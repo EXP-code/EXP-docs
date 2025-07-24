d2a4dae6972be0f54893fa62b1a6931a233b199f
Martin D. Weinberg <weinberg@astro.umass.edu>
2025-07-23 20:46:10 -0400

    Add some some print statements to debug weird missing build

--- conf.py
+++ conf.py
@@ -31,11 +31,17 @@ subprocess.run(["doxygen", doxyfile], check=True)
 
 # Build pyEXP to populate Python API documenation
 os.chdir('..')
+print('My current directory is:', os.getcwd())
+print('Beginning pyEXP build')
 if not os.path.exists('build'):
+    print('Making build directory')
     subprocess.run(["mkdir", "build"], check=True)
 os.chdir('build')
+print('My current directory is:', os.getcwd())
+print('Beginning cmake')
 subprocess.run(["cmake", "-DCMAKE_BUILD_TYPE=Release -DENABLE_USER=NO -DENABLE_NBODY=NO -DEigen3_DIR=$EIGEN_BASE/share/eigen3/cmake -Wno-dev", ".."])
 subprocess.run(["make", "-j8", "install"])
+print('Finished cmake')
 
 # Return to top level
 os.chdir(build_dir)
